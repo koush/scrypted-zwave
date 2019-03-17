bf1625f17a88ce76c2683b81591114a69e746bb3
Koushik Dutta <koushd@gmail.com>
2019-03-17 14:05:00 -0700

    es6

--- src/main.js
+++ src/main.js
@@ -1,41 +1,32 @@
-// 1) Create a new Script under Plugins.
-// 2) Set the type to Device.
-// 3) Select/check the OnOff interface.
-// 4) Save.
-// 5) Click the Debug button.
-// 6) Edit .vcode/settings.json and set the value of "scrypted.debugHost": "THIS.IS.MY.IP"
-// 7) Launch Scrypted Debugger in VS Code.
-
+// https://developer.scrypted.app/#getting-started
 import axios from 'axios';
+import sdk from "@scrypted/sdk";
+const { log } = sdk;
 
 log.i('Hello World. This will create a virtual OnOff device.');
 
-function Device() {
-}
+class Device {
+    constructor() {
+        this._isOn = false;
+    }
+    isOn() {
+        return this._isOn;
+    }
+    turnOff() {
+        // set a breakpoint here.
+        log.i('turnOff was called!');
+        this._isOn = false;
+    }
+    async turnOn() {
+        // set a breakpoint here.
+        log.i('turnOn was called!');
 
-Device.prototype.isOn = function () {
-    log.i('isOn was called!');
-    return false;
-};
-
-Device.prototype.turnOff = function () {
-    // set a breakpoint here.
-    log.i('turnOff was called!');
-};
-
-Device.prototype.turnOn = function () {
-    // set a breakpoint here.
-    log.i('turnOn was called!');
-
-    // turnOn must return immediately, but it can trigger other things... 
-    (async function () {
-        log.i('XMLHttpRequest is polyfilled by the Android host. This allows the popular http library axios or jquery ajax to work.');
-
+        log.i("Let's pretend to perform a web request on an API that would turn on a light.");
         const ip = await axios.get('http://jsonip.com');
         log.i(`my ip: ${ip.data.ip}`);
-    })();
-};
 
+        this._isOn = true;
+    }
+}
 
-
-exports.default = new Device();
+export default new Device();
